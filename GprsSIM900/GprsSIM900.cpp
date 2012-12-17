3c169b09c1823d6f3ed53405f636346b7dc10ef1
Dalmir Silva <dalmirdasilva@gmail.com>
2012-12-17 00:07:30 -0200

    Using several calls.

--- GprsSIM900/GprsSIM900.cpp
+++ GprsSIM900/GprsSIM900.cpp
@@ -32,19 +32,14 @@ unsigned char GprsSIM900::useMultiplexer(bool use) {
 }
 
 unsigned char GprsSIM900::startTask(unsigned char *apn, unsigned char *login, unsigned char *password) {
-    unsigned char i;
-    const char* parts[] = {
-        "AT+CSTT=",
-        (const char*) apn,
-        ",",
-        (const char*) login,
-        ",",
-        (const char*) password
-    };
-    for (i = 0; i < 6; i++) {
-        sim->write(parts[i]);
-    }
-    bool expected = sim->sendCommandExpecting("", "OK");
+    bool expected;
+    sim->write("AT+CSTT=");
+    sim->write((const char*) apn);
+    sim->write(",");
+    sim->write((const char*) login);
+    sim->write(",");
+    sim->write((const char*) password);
+    expected = sim->sendCommandExpecting("", "OK");
     return (unsigned char) (expected ? GprsSIM900::OK : GprsSIM900::ERROR);
 }
 
