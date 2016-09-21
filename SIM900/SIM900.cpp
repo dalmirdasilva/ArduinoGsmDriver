f9fbb838a7a60849101cd9bc97b06502c3e2a188
Dalmir da Silva <dalmirdasilva@gmail.com>
2016-09-21 15:10:16 -0300

    moved AT commands to a separeted library

--- SIM900/SIM900.cpp
+++ SIM900/SIM900.cpp
@@ -19,12 +19,10 @@ SIM900::SIM900(unsigned char receivePin, unsigned char transmitPin)
 }
 
 SIM900::SIM900(unsigned char receivePin, unsigned char transmitPin, unsigned char resetPin, unsigned char powerPin)
-        : SoftwareSerial(receivePin, transmitPin), rxBufferPos(0), echo(true), resetPin(resetPin), powerPin(powerPin), responseFullyRead(
-        true) {
-    rxBuffer[0] = '\0';
+        : SoftwareSerialAttentionDevice(receivePin, transmitPin), echo(true), resetPin(resetPin), powerPin(powerPin) {
     pinMode(resetPin, OUTPUT);
     pinMode(powerPin, OUTPUT);
-    softResetAndPowerEnabled = !(resetPin == 00 && powerPin == 0);
+    softResetAndPowerEnabled = !(resetPin == 0 && powerPin == 0);
 }
 
 SIM900::~SIM900() {
@@ -55,61 +53,6 @@ void SIM900::softPower() {
     }
 }
 
-bool SIM900::sendCommandExpecting(const char *command, const char *expectation, bool append, unsigned long timeout) {
-    if (sendCommand(command, append, timeout) == 0) {
-        return false;
-    }
-    return doesResponseContains(expectation);
-}
-
-bool SIM900::doesResponseContains(const char *expectation) {
-    return findInResponse(expectation) != NULL;
-}
-
-unsigned int SIM900::sendCommand(const char *command, bool append, unsigned long timeout) {
-    rxBufferPos = 0;
-    rxBuffer[0] = '\0';
-    flush();
-    if (append) {
-        print("AT");
-    }
-    println(command);
-    return readResponse(timeout);
-}
-
-unsigned int SIM900::readResponse(unsigned long timeout, bool append) {
-    int availableBytes;
-    unsigned long pos, last, now, start;
-    unsigned int read;
-    last = start = millis();
-    if (!append) {
-        rxBufferPos = 0;
-    }
-    pos = rxBufferPos;
-    responseFullyRead = true;
-    while (available() <= 0 && (millis() - start) < timeout)
-        ;
-    start = millis();
-    do {
-        availableBytes = available();
-        if (availableBytes > 0) {
-            if (rxBufferPos + availableBytes >= SIM900_RX_BUFFER_SIZE) {
-                availableBytes = SIM900_RX_BUFFER_SIZE - (rxBufferPos + 1);
-                responseFullyRead = false;
-            }
-            if (availableBytes > 0) {
-                last = millis();
-                // we have the guaranty that is not going to be too big because it is constrained by the buffer size.
-                read = readBytes((char *) &rxBuffer[rxBufferPos], availableBytes);
-                rxBufferPos += read;
-                rxBuffer[rxBufferPos] = '\0';
-            }
-        }
-        now = millis();
-    } while ((availableBytes > 0 || (now - last) < SIM900_SERIAL_INTERBYTE_TIMEOUT) && (now - start) < timeout && responseFullyRead);
-    return rxBufferPos - pos;
-}
-
 void SIM900::setEcho(bool echo) {
     this->echo = echo;
     char command[] = "E0";
@@ -122,28 +65,6 @@ void SIM900::setEcho(bool echo) {
 unsigned char SIM900::disconnect(DisconnectParamter param) {
     // TODO
     return 3;
-}
-
-bool SIM900::wasResponseFullyRead() {
-    return responseFullyRead;
-}
-
-const char *SIM900::findInResponse(const char *str) {
-    return strstr((const char *) &rxBuffer[0], str);
-}
-
-int SIM900::waitUntilReceive(const char *str, unsigned int timeout) {
-    const char *pos;
-    while ((pos = findInResponse(str)) == NULL && readResponse(timeout, responseFullyRead) > 0)
-        ;
-    if (pos != NULL) {
-        return (int) (pos - (const char *) &rxBuffer[0]);
-    }
-    return -1;
-}
-
-void SIM900::discardBuffer() {
-    rxBuffer[0] = '\0';
 }
 
 #endif /* __ARDUINO_DRIVER_GSM_SIM900_CPP__ */
--- SIM900/SIM900.h
+++ SIM900/SIM900.h
@@ -12,28 +12,12 @@
 #define __ARDUINO_DRIVER_GSM_SIM900_H__ 1
 
 #include <Arduino.h>
+#include <SoftwareSerialAttentionDevice.h>
 #include <string.h>
-#include <SoftwareSerial.h>
 
-#define SIM900_RX_BUFFER_SIZE 256
-
-#define SIM900_DEFAULT_COMMAND_RESPONSE_TIMEOUT 1000UL
 #define SIM900_INITIALIZATION_TIMEOUT           10000UL
-#define SIM900_SERIAL_INTERBYTE_TIMEOUT         50UL
-#define SIM900_SERIAL_FIRSTBYTE_TIMEOUT         5000UL
 
-
-class SIM900: public SoftwareSerial {
-
-    /**
-     * RX buffer.
-     */
-    unsigned char rxBuffer[SIM900_RX_BUFFER_SIZE];
-
-    /**
-     * RX buffer position.
-     */
-    unsigned int rxBufferPos;
+class SIM900: public SoftwareSerialAttentionDevice {
 
     /**
      * Using echo.
@@ -49,12 +33,6 @@ class SIM900: public SoftwareSerial {
      * Soft power on/off pin.
      */
     unsigned char powerPin;
-
-    /**
-     * Bool indicating the last command's response was fully read by
-     * readResponse method.
-     */
-    bool responseFullyRead;
 
     /**
      * Enable soft power and reset
@@ -135,135 +113,6 @@ public:
     void softPower();
 
     /**
-     * Get a pointer to the last response.
-     * 
-     * @return 
-     */
-    unsigned char *getLastResponse() {
-        return &rxBuffer[0];
-    }
-
-    /**
-     * Sends a command expecting some result.
-     * 
-     * @param command           The command string, should be \0 ended.
-     * @param expectation       The expectation string.
-     * @param timeout           The maximum time to perform the op.
-     * @return 
-     */
-    bool sendCommandExpecting(const char *command, const char *expectation, bool append, unsigned long timeout);
-
-    /**
-     * Sends a command expecting some result.
-     * 
-     * @param   command             The command string, should be \0 ended.
-     * @param   expectation         The expectation string.
-     * @param   append              If should append AT+ in the command.
-     * @return 
-     */
-    inline bool sendCommandExpecting(const char *command, const char *expectation, bool append) {
-        return sendCommandExpecting(command, expectation, append, SIM900_DEFAULT_COMMAND_RESPONSE_TIMEOUT);
-    }
-
-    /**
-     * Sends a command expecting some result.
-     * 
-     * @param   command             The command string, should be \0 ended.
-     * @param   expectation         The expectation string.
-     * @param   timeout             Timeout.
-     * @return 
-     */
-    inline bool sendCommandExpecting(const char *command, const char *expectation, unsigned long timeout) {
-        return sendCommandExpecting(command, expectation, false, timeout);
-    }
-
-    /**
-     * Sends a command expecting some result.
-     * 
-     * @param command           The command string, should be \0 ended.
-     * @param expectation       The expectation string.
-     * @return 
-     */
-    inline bool sendCommandExpecting(const char *command, const char *expectation) {
-        return sendCommandExpecting(command, expectation, (bool) false);
-    }
-
-    /**
-     * Checks if the last response contains the given sub-string.
-     * 
-     * @param expectation       The expectation string.
-     * @return 
-     */
-    bool doesResponseContains(const char *expectation);
-
-    /**
-     * Sends a command to the device.
-     * 
-     * @param command           The command string, should be \0 ended.
-     * @param timeout           The maximum time to perform the op.
-     * @return 
-     */
-    unsigned int sendCommand(const char *command, bool append, unsigned long timeout);
-
-    /**
-     * Sends a command to the device.
-     * 
-     * @param command           The command string, should be \0 ended.
-     * @param append            Boolean saying if the AT must be appended.
-     * @return 
-     */
-    inline unsigned int sendCommand(const char *command, bool append) {
-        return sendCommand(command, append, 1000);
-    }
-
-    /**
-     * Sends a command to the device.
-     * 
-     * @param command           The command string, should be \0 ended.
-     * @param append            Boolean saying if the AT must be appended.
-     * @return 
-     */
-    inline unsigned int sendCommand(const char *command, unsigned long timeout) {
-        return sendCommand(command, (bool) false, timeout);
-    }
-
-    /**
-     * Sends a command to the device.
-     * 
-     * @param command           The command string, should be \0 ended.
-     * @return 
-     */
-    inline unsigned int sendCommand(const char *command) {
-        return sendCommand(command, (bool) false);
-    }
-
-    /**
-     * Sends a command to the device.
-     */
-    inline unsigned int sendCommand() {
-        return sendCommand("");
-    }
-
-    /**
-     * Reads the response from the device.
-     * 
-     * @param timeout           The maximum time to perform the op.
-     * @return                  How many bytes was received. 0 if timeout.
-     */
-    inline unsigned int readResponse(unsigned long timeout) {
-        return readResponse(timeout, false);
-    }
-
-    /**
-     * Reads the response from the device.
-     *
-     * @param timeout           The maximum time to perform the op.
-     * @param append            Append the response in the internal buffer.
-     * @return                  How many bytes was received. 0 if timeout.
-     */
-    unsigned int readResponse(unsigned long timeout, bool append);
-
-    /**
      * Configures echo mode
      * 
      * @param echo
@@ -271,40 +120,6 @@ public:
     void setEcho(bool echo);
 
     unsigned char disconnect(DisconnectParamter param);
-
-    /**
-     * Checks if the rx is fully read.
-     *
-     * If false, a new call to readResponse will read more data from tx
-     * placing the new data over the current data in it. Consecutive calls to
-     * readResponse will write data into the same buffer.
-     *
-     * use getLastResponse to consume the current data and then call readResponse again
-     * in case tx was not fully read.
-     */
-    bool wasResponseFullyRead();
-
-    /**
-     * Searches the response for a given string.
-     *
-     * @param   str     The string to be searched inside response
-     * @return          The pointer to the first found string, NULL if not found
-     */
-    const char *findInResponse(const char *str);
-
-    /**
-     * Keeps reading the response until finds the str or timeout.
-     *
-     * @param   str         String it tries to find
-     * @param   timeout     Timeout in millis
-     * @return              The position to the first char it finds in the internal buffer, -1 otherwise
-     */
-    int waitUntilReceive(const char *str, unsigned int timeout);
-
-    /**
-     * Discards the internal buffer.
-     */
-    void discardBuffer();
 
     /*
      void getProductIdentificationInformation();
